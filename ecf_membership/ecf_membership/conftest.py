0a2556e5729bb99493c533e10f43353e0010bdc7
ArtusU <artusrock@hotmail.com>
2021-03-10 09:53:43 +0000

    first commit

---
+++ ecf_membership/ecf_membership/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ecf_membership.users.models import User
+from ecf_membership.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
